972a758c03720dcdc92ca37ee8de41600122382e
Yoel Cabaleiro <113912282+Yoel-Cabaleiro@users.noreply.github.com>
2022-09-20 15:31:38 +0100

    Update functions practice (combat).py

--- functions practice (combat).py
+++ functions practice (combat).py
@@ -6,13 +6,13 @@ def combat_menu():
     global characterLife
     for key, action in combatMenu.items():
         print(key, action)
-    takeAction = int(input("What do you want to do? "))
+    takeAction = int(input("What do you want to do? \n "))
     if takeAction == 1:
         character_combat()
     if takeAction == 2:
         print("You embrace a defensive position. You heal up 25 HP")
         characterLife += 25
-        print("Your life now is " + str(characterLife))
+        print("Your life now is " + str(characterLife) + "\n ")
             
 
 def guardian_combat():
@@ -22,15 +22,15 @@ def guardian_combat():
     if attack >= 60:
         print("Guardian hits you with his sword")
         characterLife -= 10
-        print("Your life now is " + str(characterLife))
+        print("Your life now is " + str(characterLife) + "\n ")
     if attack >= 10 and attack < 60:
         print("Guardian hits you with a heavy thrust")
         characterLife -= 20
-        print("Your life now is " + str(characterLife))
+        print("Your life now is " + str(characterLife) + "\n ")
     if attack < 10:
         print("Guardian fiercely swing his sword towards your face. It's a critical hit!")
         characterLife -= 40
-        print("Your life now is " + str(characterLife))
+        print("Your life now is " + str(characterLife) + "\n ")
         
 
 def character_combat():
@@ -38,10 +38,10 @@ def character_combat():
     import random
     attack = random.randint(1, 100)
     if attack >= 90:
-        print("You unleash a mighty blow with your axe. It's a critical hit!")
+        print("You unleash a mighty blow with your axe. It's a critical hit!\n ")
         guardianLife -= 20
     if attack < 90:
-        print("You hit your foe with your axe")
+        print("You hit your foe with your axe\n ")
         guardianLife -= 10
 
 
